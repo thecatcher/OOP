bcd450f600148c261abaeccc128e7b6bad83ead5
Brady.Y.Gao <gaoyang1019@hotmail.com>
2020-01-20 17:42:26 +0800

    202001201742

---
+++ 面向对象基础/10_不使用继承开发动物和狗.py
@@ -0,0 +1,30 @@
+class Animal:
+    def eat(self):
+        print("eat")
+
+    def run(self):
+        print("run")
+
+    def drink(self):
+        print("drink")
+
+    def sleep(self):
+        print("sleep")
+
+
+class Dog:
+
+    def eat(self):
+        print("eat")
+
+    def run(self):
+        print("run")
+
+    def drink(self):
+        print("drink")
+
+    def sleep(self):
+        print("sleep")
+
+    def bark(self):
+        print("wang!wang!wang!")
